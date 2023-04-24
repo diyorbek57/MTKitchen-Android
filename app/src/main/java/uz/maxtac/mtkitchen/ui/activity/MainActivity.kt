bf76f877656ceb695adaf5c78621a0afa170af96
diyorbek57 <hosmerdiyir57@gmail.com>
2023-04-25 00:57:30 +0500

    created bottom navigation and selectors

--- app/src/main/java/uz/maxtac/mtkitchen/ui/activity/MainActivity.kt
+++ app/src/main/java/uz/maxtac/mtkitchen/ui/activity/MainActivity.kt
@@ -3,10 +3,17 @@ package uz.maxtac.mtkitchen.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import uz.maxtac.mtkitchen.R
+import uz.maxtac.mtkitchen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+      binding.bottomNavigationView.background = null  //Removing the background shadow
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
